d9f4c37beb3b56c02e51656bbb26d04d9ce359e0
m1nified <m1nified.dev@gmail.com>
2019-09-02 11:17:41 +0200

    fixed panels

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,46 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import './App.scss';
 import PanelsList from './components/panelsList/PanelsList';
+import AdminsList from './components/admins/list/AdminsList';
+import AdminCreate from './components/admins/create/AdminCreate';
+import UsersList from './components/users/list/UsersList';
+import UserCreate from './components/users/create/UserCreate';
+import InventoriesList from './components/inventories/list/InventoriesList';
+import InventoryCreate from './components/inventories/create/InventoryCreate';
+import ProductsList from './components/products/list/ProductsList';
+import ProductCreate from './components/products/create/ProductCreate';
+import CategoriesList from './components/categories/list/CategoriesList';
+import CategoryCreate from './components/categories/create/CategoryCreate';
+import CategoryDetails from './components/categories/details/CategoryDetails';
+import OrdersList from './components/orders/list/OrdersList';
+import OrderDetails from './components/orders/details/OrderDetails';
+import OrderCreate from './components/orders/create/OrderCreate';
+import OrderProductsList from './components/orderProducts/list/OrderProductsList';
 
 const App: React.FC = () => {
   return (
     <div className="App container">
       <Router>
-        {/* <PanelsList /> */}
+        {/* <Route path="/" component={} /> */}
+        <Route path="/adminPanels" component={PanelsList} />
+        <Route path="/admins" component={AdminsList} />
+        <Route path="/adminAdd" component={AdminCreate} />
+        <Route path="/users" component={UsersList} />
+        <Route path="/userAdd" component={UserCreate} />
+        <Route path="/inventories" component={InventoriesList} />
+        <Route path="/inventoryAdd" component={InventoryCreate} />
+        <Route path="/products" component={ProductsList} />
+        <Route path="/productAdd" component={ProductCreate} />
+        <Route path="/categories" component={CategoriesList} />
+        <Route path="/categoryAdd" component={CategoryCreate} />
+        <Route path="/category/:categoryId" component={CategoryDetails} />
+        <Route path="/orders" component={OrdersList} />
+        <Route path="/order/:orderId" component={OrderDetails} />
+        <Route path="/orderAdd" component={OrderCreate} />
+        <Route path="/orderProducts" component={OrderProductsList} />
+        <Route path="/orderProductAdd" component={PanelsList} />
       </Router>
     </div>
   );
--- app/src/components/panelsList/PanelsList.tsx
+++ app/src/components/panelsList/PanelsList.tsx
@@ -1,20 +1,5 @@
-import React, { Component, FormEvent } from "react";
-import { Link, Route } from "react-router-dom";
-import AdminsList from "../admins/list/AdminsList";
-import AdminCreate from "../admins/create/AdminCreate";
-import UsersList from "../users/list/UsersList";
-import UserCreate from "../users/create/UserCreate";
-import InventoriesList from "../inventories/list/InventoriesList";
-import InventoryCreate from "../inventories/create/InventoryCreate";
-import ProductsList from "../products/list/ProductsList";
-import ProductCreate from "../products/create/ProductCreate";
-import CategoriesList from "../categories/list/CategoriesList";
-import CategoryCreate from "../categories/create/CategoryCreate";
-import CategoryDetails from "../categories/details/CategoryDetails";
-import OrdersList from "../orders/list/OrdersList";
-import OrderDetails from "../orders/details/OrderDetails";
-import OrderCreate from "../orders/create/OrderCreate";
-import OrderProductsList from "../orderProducts/list/OrderProductsList";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class PanelsList extends Component {
 
@@ -50,23 +35,6 @@ class PanelsList extends Component {
                     <Link to="/orderProducts">OrderProducts list</Link>
                     <Link to="/orderProductAdd">OrderProduct add</Link>
                 </div>
-                {/* <Route path="/" component={} /> */}
-                <Route path="/admins" component={AdminsList} />
-                <Route path="/adminAdd" component={AdminCreate} />
-                <Route path="/users" component={UsersList} />
-                <Route path="/userAdd" component={UserCreate} />
-                <Route path="/inventories" component={InventoriesList} />
-                <Route path="/inventoryAdd" component={InventoryCreate} />
-                <Route path="/products" component={ProductsList} />
-                <Route path="/productAdd" component={ProductCreate} />
-                <Route path="/categories" component={CategoriesList} />
-                <Route path="/categoryAdd" component={CategoryCreate} />
-                <Route path="/category/:categoryId" component={CategoryDetails} />
-                <Route path="/orders" component={OrdersList} />
-                <Route path="/order/:orderId" component={OrderDetails} />
-                <Route path="/orderAdd" component={OrderCreate} />
-                <Route path="/orderProducts" component={OrderProductsList} />
-                <Route path="/orderProductAdd" component={PanelsList} />
             </div>
         )
     }
